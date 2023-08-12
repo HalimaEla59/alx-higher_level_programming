3386c2141ff9eadd2206169eb34998e7d6caea80
HalimaEla59 <halimaelabbadi@gmail.com>
2023-08-11 18:26:11 -0700

    2-args.py created

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+i#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) == 1:
+        print("0 arguments.")
+    elif len(argv) == 2:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(len(argv) - 1))
+    for i in range(len(argv)):
+        print("{:d}: {:s}".format(i, argv[i]))
