817de5ff3ea94e7aeddadbe5d80428076e3f4ee0
HalimaEla59 <halimaelabbadi@gmail.com>
2023-08-14 19:12:41 -0700

    10-divisible_by_2.py created

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new = my_list[:]
+    for i in range(len(new)):
+        if new % 2 == 0:
+            new[i] = True
+        else:
+            new[i] = False
+    return new
