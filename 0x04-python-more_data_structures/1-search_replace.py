ea271e2ac03189809ef72a00b5146d48a880b0da
HalimaEla59 <halimaelabbadi@gmail.com>
2023-08-16 14:17:55 -0700

    1-...

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    res = my_list[:]
+    for i range(len(res)):
+        if res[i] == search:
+            res[i] = replace
+    return res
