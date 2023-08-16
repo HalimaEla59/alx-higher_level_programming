b6c1127d974139ef0940e3c1fc31279309688620
HalimaEla59 <halimaelabbadi@gmail.com>
2023-08-16 14:21:55 -0700

    1-... fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    res = my_list[:]
-    for i range(len(res)):
-        if res[i] == search:
-            res[i] = replace
-    return res
+    return [replace if i is search else i for i in my_list]
