5bbc7245ec2ee0a57305734cdb241a6b0a8b0c5e
HalimaEla59 <halimaelabbadi@gmail.com>
2023-08-16 17:37:29 -0700

    fix?

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/pthon3
 def multiply_by_2(a_dictionary):
     if a_dictionary:
-        return dict((k, v * 2) for k, v in a_dictionary.items())
+        return {k: (v * 2) for k, v in a_dictionary.items()}
