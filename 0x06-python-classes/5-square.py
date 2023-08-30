1b07b3efb8007f43e21eebd1e5512a92a2115d9d
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2023-08-30 02:42:11 +0100

    starting on 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+
+"""define a class named Square"""
+
+
+class Square:
+    """instantination with optional size"""
+
+    def __init__(self, size=0):
+        """Initializes a new square
+
+        Args:
+           __size(int): size of the square
+           must be positive and integer
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """getter the size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter of size of square
+        value must be positive and int
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+  
+    def area(self):
+        """returns the area of square"""
+        return (self.__size * self.__size)
