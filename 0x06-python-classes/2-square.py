9a6bb9be3cbfacd3d861f72756964cf28cbe335c
HalimaEla59 <halimaelabbadi@gmail.com>
2023-08-29 18:05:23 -0700

    comment

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,8 +4,7 @@
 
 
 class Square:
-    """the same as the previous with 
-    instantination with optional size"""
+    """instantination with optional size"""
 
     def __init__(self, size=0):
         """Initializes a new square
