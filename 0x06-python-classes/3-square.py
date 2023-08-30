981505cb56fedbbddc4bfee394e1a5a30e83d046
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2023-08-30 02:09:22 +0100

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
 
+"""define a class named Square"""
+
+
+class Square:
+    """instantination with optional size"""
+
+    def __init__(self, size=0):
+        """Initializes a new square
+
+        Args:
+           __size(int): size of the square
+           must be positive and integer
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
