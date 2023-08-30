9d0bbe5fc481c49dede4579af75f8044eff16d6c
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2023-08-30 02:23:41 +0100

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+"""define a class named Square"""
+
+
+class Square:
+    """instantination with optional size"""
+
+    def __init__(self, size=0):
+        """Initializes a new square
+
+        Args:
+           __size(int): size of the square
+           must be positive and integer
+        """
+        self.__size = size
+    @property
+  
+
+    def area(self):
+        """returns the area of square"""
+        return (self.__size * self.__size)
