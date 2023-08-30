d92b473505faf3384faef7a8839329ae3d4db40f
HalimaEla59 <halimaelabbadi@gmail.com>
2023-08-29 17:47:32 -0700

    1-square.py added

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""class named square"""
+
+class Square:
+    """with privat instance attribute: size"""
+
+    def __init__(self, size):
+        self.__size = size
+        """Initialize a new square with:
+
+        args:
+           --size(int): size of the square
+        """
