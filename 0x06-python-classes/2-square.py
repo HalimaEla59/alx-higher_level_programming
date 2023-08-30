7a3132ed5d872ea1ac894e8f2a1523626a79c985
HalimaEla59 <halimaelabbadi@gmail.com>
2023-08-29 18:03:54 -0700

    2-square.py added

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""define a class named Square"""
+
+
+class Square:
+    """the same as the previous with 
+    instantination with optional size"""
+
+    def __init__(self, size=0):
+        """Initializes a new square
+
+        Args:
+           __size(int): size of the square
+           must be positive and integer
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
