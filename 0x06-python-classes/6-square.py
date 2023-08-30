a59d3eba76b505f8139eed88dc7555efb91d27a7
HalimaEla59 <halimaelabbadi@gmail.com>
2023-08-29 19:20:29 -0700

    6-square.py continued

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -2,17 +2,19 @@
 
 """define a class named Square"""
 
+
 class Square:
     """instantination with optional size"""
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """Initializes a new square
 
         Args:
-           __size(int): size of the square
-           must be positive and integer
+           size(int): size of the square
+           position(int, int): the position
         """
-        self.__size = size
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -27,15 +29,33 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    @property
+    def position(self):
+        """getter of position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """setter of position"""
+        if ((type(value) is not tuple) or (len(value) is not 2) or
+                (type(value[0]) is not int) or
+                (type(value[1]) is not int)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if (value[0] < 0) or (value[1] < 0):
+            raise ValueError("position must be a tuple of 2 positive integers")
+        self.__position = value
   
     def area(self):
         """returns the area of square"""
         return (self.__size * self.__size)
 
     def my_print(self):
-        """prints size"""
-        for i in range(self.__size):
-            print("#" * self.__size, end="")
+        """prints square of # with position and size"""
+        if self.size == 0:
             print()
-        if self.__size == 0:
+            return
+        for i in range(self.position[1]):
             print()
+        for i in range(self.size):
+            print("{}{}".format(" " * self.position[0], "#" * self.size))
