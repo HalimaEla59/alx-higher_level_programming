145bd6cffac861592d48596f46e2c6a5807db3d8
HalimaEla59 <halimaelabbadi@gmail.com>
2023-09-04 16:28:54 -0700

    continue working on 8-...

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -62,6 +62,21 @@ class Rectangle:
             return 0
         return (self.__height * 2) + (self.__width * 2)
 
+    def bigger_or_equal(rect_1, rect_2):
+        """Returns the rectangle with bigger area
+
+        Args:
+            rect_1 (Rectangle): first rectangle
+            rect_2 (Rectangle): second rectangle
+        """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area:
+            return rect_1
+        return rect_2
+
     def __str__(self):
         """Prints the rectangle with hashtags"""
         if not self.perimeter():
