5c9cc743511efe07f686cfab29eb64495658a192
HalimaEla59 <halimaelabbadi@gmail.com>
2023-09-11 16:05:18 -0700

    4-... fixed

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,6 +4,6 @@
 
 def inherits_from(obj, a_class):
     """returns True or False"""
-    if tpe(obj) != a_class and issubclass(type(obj), a_class):
+    if type(obj) != a_class and issubclass(type(obj), a_class):
         return True
     return False
