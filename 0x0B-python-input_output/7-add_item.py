83af6d02465f7aab67753b87c07dfb156d8161e2
HalimaEla59 <halimaelabbadi@gmail.com>
2023-09-12 16:48:22 -0700

    7-... style

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,8 +2,6 @@
 """adds all args to python list and saves them as file"""
 import sys
 import json
-
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
