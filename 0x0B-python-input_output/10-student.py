73f5c8f9249414d3e452be4285e57573d1e9f238
HalimaEla59 <halimaelabbadi@gmail.com>
2023-09-12 17:18:40 -0700

    fix

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -18,6 +18,6 @@ class Student:
         in attrs must be retrieved
         """
         if (type(attrs) == list and all(type(i) == str for i in attrs)):
-            return ({i: j for i, jin self.__dict__.items() if i in attrs})
+            return ({i: getattr(self, i) for i in attrs if hasattr(self, i)})
         else:
             return self.__dict__
