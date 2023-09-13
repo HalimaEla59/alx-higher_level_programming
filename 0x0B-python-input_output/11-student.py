2ba0a4916a4f861df90fde2c413d5cfffe7eacfa
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2023-09-13 01:20:46 +0100

    11-.. starting with 10-..

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""class that defines a Student"""
+
+
+class Student:
+    """representing a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initialising a student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary represenation of student
+
+        if attrs is list of strings, only what is contained
+        in attrs must be retrieved
+        """
+        if (type(attrs) == list and all(type(i) == str for i in attrs)):
+            return ({i: getattr(self, i) for i in attrs if hasattr(self, i)})
+        else:
+            return self.__dict__
