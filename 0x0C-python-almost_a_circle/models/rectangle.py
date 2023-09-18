74aba8abfebc60febe3fe50cc9f5bf6e3554ba80
HalimaEla59 <halimaelabbadi@gmail.com>
2023-09-18 11:19:13 -0700

    continuing rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -45,3 +45,56 @@ class Rectangle(Base):
     def x(self):
         """getter of x"""
         return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ setter of x"""
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """getter of y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """setter of y"""
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """returns the area of rectangle"""
+        return self.width * self.height
+
+    def display(self):
+        """prints a rectangle of #"""
+        print('\n' * self.y, end="")
+        print(''.join(' ' * self.x + '#' * self.width + '\n'
+            for i in range(self.height)), end="")
+
+    def __str__(self):
+        """str represntation of the rectangle"""
+        return "[{}] ({}) {}/{} - {}/{}".format(
+                type(self).__name__, self.id,
+                self.x, self.y,
+                self.width, self.height)
+
+    def update(self, *args, **kwargs):
+        """updates the rectangle"""
+        if args and len(args) != 0:
+            count = 0
+            keys = ['id', 'width', 'height', 'x', 'y']
+            for arg in args:
+                setattr(self, keys[count], arg)
+                count += 1
+        elif kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
