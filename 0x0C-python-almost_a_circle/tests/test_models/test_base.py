cdf3afa4ebaf0a9cca50cf06795ce949c26126d1
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2023-09-18 19:30:48 +0100

    test_base.py added

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import unittest
+import os.path
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+"""tests the Base"""
+
+
+class TestBase(unittest.TestCase):
+
+if __name__ == '__main__':
+    unittest.main()
