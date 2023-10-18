dd4e6bb74f28227e0cb0f2c49b90055266106036
HalimaEla59 <halimaelabbadi@gmail.com>
2023-10-17 18:29:43 -0700

    16-.. fix

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,5 @@
 -- lists records of second_table Don’t list rows without a name val
-SELECT score, name FROM second_table WHERE name <> ''
-GROUP BY score DESC, name DESC;
+SELECT 'score', 'name'
+FROM 'second_table'
+WHERE 'name' != ""
+ORDER BY 'score' DESC;
