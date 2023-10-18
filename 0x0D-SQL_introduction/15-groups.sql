19c1f037a0cad97540e64b9f0fd3e7d792424704
HalimaEla59 <halimaelabbadi@gmail.com>
2023-10-17 18:36:33 -0700

    15-. again

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,5 @@
 -- lists number of records with same score (descending)
-SELECT 'score', COUNT('score') AS 'number' FROM 'second_table'
-GROUP BY 'score' DESC;
+SELECT 'score', COUNT(*) AS 'number'
+FROM 'second_table'
+GROUP BY 'score'
+ORDER BY 'number' DESC;
