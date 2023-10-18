eecc8bcdf1ebe6d832136542c866b48029981c55
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2023-10-18 02:20:00 +0100

    Update 15-groups.sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
--- lists the number of records with same score in second_table (descending)
+-- lists number of records with same score (descending)
 SELECT score, COUNT(score) AS number FROM second_table
 GROUP BY score DESC;
