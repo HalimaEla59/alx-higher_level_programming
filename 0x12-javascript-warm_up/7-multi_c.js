7cb5b82a257a9c82193db58a3ccbf31ac6390b3d
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2023-11-13 23:41:13 +0100

    7-... added

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+let s = process.argv[2];
+if (isNaN(Number(s))) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < Number(s); i++) {
+    console.log('C is fun');
+  }
+}
