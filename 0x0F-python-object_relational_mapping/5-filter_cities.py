a7c75497f2cb21ce0c6f163053ee394641bc5ac3
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2023-11-19 01:09:51 +0100

    5* created

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""takes in the name of a state as an argument and lists all cities
+of that state, using the database hbtn_0e_4_usa"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name +
+    FROM cities JOIN states ON cities.state_id = states.id +
+    WHERE states.name = '{}';".format(sys.argv[4]))
+    stat = cur.fetchall()
+    print(", ".join([row[0] for row in stat]))
+    cur.close()
+    conn.close()
