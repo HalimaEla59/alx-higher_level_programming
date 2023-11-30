9af66dd0318c85f5372d9c766168f16ed40adef1
HalimaEla59 <halimaelabbadi@gmail.com>
2023-11-30 15:43:50 -0800

    task 6 updated

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,15 +1,22 @@
+#!/usr/bin/python3
+"""Finds a peak in an unsorted list of integers"""
+
+
 def find_peak(list_of_integers):
+    """There might be more than one peak"""
     if not list_of_integers:
         return None
 
-    low, high = 0, len(list_of_integers) - 1
+    low = 0
+    high = len(list_of_integers) - 1
 
     while low <= high:
         mid = (low + high) // 2
         mid_value = list_of_integers[mid]
 
         if (mid == 0 or mid_value >= list_of_integers[mid - 1]) and \
-           (mid == len(list_of_integers) - 1 or mid_value >= list_of_integers[mid + 1]):
+           (mid == len(list_of_integers) - 1 or \
+           mid_value >= list_of_integers[mid + 1]):
             return mid_value
         elif mid > 0 and mid_value < list_of_integers[mid - 1]:
             high = mid - 1
