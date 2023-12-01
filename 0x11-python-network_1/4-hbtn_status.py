8d875310ad139621eb737d9c0078ebc049109fe8
HalimaEla59 <halimaelabbadi@gmail.com>
2023-12-01 09:18:09 -0800

    typo

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,7 +3,7 @@
 import requests
 
 if __name__ == "__main__":
-    req = request.get('https://alx-intranet.hbtn.io/status')
+    req = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(req.text)))
     print("\t- content: {}".format(req.text))
