7f7a1a99cb06bbde7aa1243295f7209b8287350d
HalimaEla59 <halimaelabbadi@gmail.com>
2023-12-01 14:30:14 -0800

    task 8 added

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Takes in a letter and sends a POSTrequest to http://0.0.0.0:5000..."""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+    http = "http://0.0.0.0:5000/search_user"
+    req = requests.post(http, data={'q': letter})
+    try:
+        if req.json():
+            reqId = req.json().get('id')
+            reqName = req.json().get('name')
+            print("[{}] {}".format(reqId, reqName))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
