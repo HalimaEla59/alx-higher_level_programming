1748c1bcfc057c87ff34beca7c79f1311ebbf533
HalimaEla59 <halimaelabbadi@gmail.com>
2023-12-01 14:08:26 -0800

    task 7 added

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Displays the body of the response with the error codes"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = requests.get(url)
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
