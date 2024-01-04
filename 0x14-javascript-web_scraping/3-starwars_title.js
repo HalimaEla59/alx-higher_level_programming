d5f5d00b794dd825228fee54cf68fb76fbd901b5
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2024-01-04 01:44:59 +0100

    Update 3-.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], function (error, response, body) {
+request('https://swapi-api.alx-tools.com/api/films/' + process.argv[1], function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
