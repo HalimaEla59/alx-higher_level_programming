d3612b30a3e77c8e588c4ec573a237ecda5badfc
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2024-01-04 01:32:21 +0100

    3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const ar = process.argv[2];
+const request = require('request');
+request('http://swapi.co/api/films/' + ar, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body)['title']);
+  }
+});
