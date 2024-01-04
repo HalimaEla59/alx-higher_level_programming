ed38f00761069f59dac9731cce50b92865db2de2
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2024-01-04 01:55:36 +0100

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+const URL = process.argv[2];
+request(URL, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    let results = JSON.parse(body).results;
+    let count = 0;
+    for (let i in results) {
+      for (let chr of results[i].characters) {
+        if (chr.search('/18/') > 0) { count += 1; }
+      }
+    }
+    console.log(count);
+  }
+});
